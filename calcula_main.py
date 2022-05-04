077469cbc81e2d81ea4d14e371cb1419b31e701c
Pescador15 <103229809+Pescador15@users.noreply.github.com>
2022-05-03 22:53:50 -0300

    Update calcula_main.py

--- calcula_main.py
+++ calcula_main.py
@@ -8,10 +8,6 @@ def insere_entradas(con, valor, data, nome, recorrente):
     valor_entradas = float(input('Valor da conta:'))
     data_entradas = str(input('Digite a data: '))
     data_format = datetime.strptime(data_entradas,'%d-%m-%Y')
-    
-
-    
-    
     nome_entradas = str(input('Nome da conta: '))
     recorrente_entradas = str(input('Recorrente: ')).strip().upper()
     if recorrente_entradas in 'Ss':
@@ -31,7 +27,7 @@ def insere_saidas(con, valor, vencimento, nome, recorrente):
 
     valor_saida = float(input('Valor da saida: '))
     data_saida = str(input('Data da saida: '))
-    data_format1 = datetime.strptime(data_saida, '%d-%m%Y')
+    data_format1 = datetime.strptime(data_saida, '%d-%m-%Y')
     nome_saida = str(input('Descrição da saida: '))
     recorrente_saida = str(input('recorrente: ')).strip().upper()
     if recorrente_saida in 'sS':
@@ -49,25 +45,37 @@ def insere_saidas(con, valor, vencimento, nome, recorrente):
 def consulta_entradas(con, valor, data, nome, recorrente):
     
     consulta_entradas1 = 'SELECT * FROM entradas'
-   
-
     cursor = con.cursor()
-    cursor.execute(consulta_entradas1,datas)
-    
+    cursor.execute(consulta_entradas1)
     colunas = cursor.fetchall()
     for coluna in colunas:
         print(valor,'valor:',coluna[0],'R$')
         print(data, coluna[1])
         print(nome,'nome:', coluna[2])
         print(recorrente, coluna[3])
-        
-    
+
+
+def consulta_saidas(con, valor, vencimento, nome, recorrente):
+
+    consulta_saidas1 = 'SELECT * FROM saidas'
+    cursor = con.cursor()
+    cursor.execute(consulta_saidas1)
+    colunas = cursor.fetchall()
+    for coluna in colunas:
+        print(valor, 'valor:',coluna[0],'R$')
+        print(vencimento,coluna[1])
+        print(nome,'nome:',coluna[2])
+        print(recorrente,coluna[3])
+
+
 def main():
+   
     con = criar_conexao("localhost", "root", "", "calcula_python")
     print('''Digite a opção desejada:
     [1] entradas
     [2] saidas
-    [3] consulta de dados
+    [3] consulta de dados de entrada
+    [4] consulta de dados de saída
     ''')
     opcao = int(input('Qual opção você deseja? '))
     if opcao == 1:
@@ -76,6 +84,8 @@ def main():
         insere_saidas(con, '', '', '', '')
     elif opcao == 3:
         consulta_entradas(con,'' ,'' , '' , '')
+    elif opcao == 4: 
+        consulta_saidas(con, '', '', '', '')
     
 
     
