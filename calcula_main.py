2fc46143460a77e622e6d5927bd8e047ef9a0c1a
cassiano <cassianowatte@gmail.com>
2022-06-30 11:37:58 -0300

    30-06-2022

--- calcula_main.py
+++ calcula_main.py
@@ -1,178 +1,184 @@
+# @author cassiano watte | cassianowatte@gmail.com
+# @description this is a study case
+
+# used because
 import pandas as pd
-from datetime import datetime
-from calcula_conexao import criar_conexao, fechar_conexao
+import os
+import csv
+
+coloumns_names = [
+    'Valor', 'Nome', 'Data', 'Recorrente'
+]
+coloumns_names2 = [
+    'Valor', 'Nome', 'Data', 'Pago' 
+]
 
 
+class ContentTable:
 
-def insere_entradas(con, valor, data, nome, recorrente):
+    def inputData(self, entrada=None, nome=None, data=None, recorrente=None, saida=None):
+        if entrada is not None:
+            self.user_input = entrada
+        if nome is not None:
+            self.user_name = nome
+        if data is not None:
+            self.user_date = data
+        if recorrente is not None:
+            self.user_continuous = recorrente
+        if saida is not None:
+            self.user_output = saida
+        
+
+    def string_format(self):
+        if(hasattr(self, 'user_output') == False):
+            return '{} , {} , {} , {}'.format(self.user_input, self.user_name, self.user_date, self.user_continuous)
+        return '{} , {} , {} , {}'.format(self.user_output, self.user_name, self.user_date, self.user_continuous)
+
+    def getListFormat(self):
+        if(hasattr(self, 'user_output') == False):
+            return [self.user_input, self.user_name, self.user_date, self.user_continuous]
+        return [self.user_output, self.user_name, self.user_date, self.user_continuous]
+    
+    
+
+
+def insert_compensation(option):
     valor_entradas = float(input('Valor da conta:'))
     data_entradas = str(input('Digite a data: '))
-    data_format = datetime.strptime(data_entradas, '%d-%m-%Y')
     nome_entradas = str(input('Nome da Entrada: '))
-    recorrente_entradas = str(input('Recorrente: ')).strip().upper()
+    recorrente_entradas = str(input('Recorrente [S/N]: ')).strip().upper()
     if recorrente_entradas in 'Ss':
         recorrente_entradas = 'sim'
     else:
         recorrente_entradas = 'nao'
-    cursor = con.cursor()
-    entradas_sql = "INSERT INTO ENTRADAS (valor, data, nome, recorrente) values (%s, %s, %s, %s)"
-    input_entradas = (valor_entradas, data_format,nome_entradas, recorrente_entradas)
-    cursor.execute(entradas_sql, input_entradas)
-    con.commit()
+    tabela = ContentTable()
+    tabela.inputData(valor_entradas, nome_entradas,
+                     data_entradas, recorrente_entradas)
+    # if data.csv doesn't exist create it
+    diretorio = ('compensation.csv')
+    if os.path.isfile(diretorio) == False:
+        with open('compensation.csv', 'w') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            writer.writerow((coloumns_names))
+            csvfile.write('\n')
+            csvfile.close()
+    csvFormat = pd.read_csv('compensation.csv', sep=',')
+    csvFormat = pd.concat([csvFormat, pd.DataFrame(
+        [tabela.getListFormat()], columns=coloumns_names)])
+    csvFormat.to_csv('compensation.csv', index=False)
     print('valores de entradas inseridos com sucesso!')
-    cursor.close()
 
 
-def insere_saidas(con, valor, vencimento, nome, recorrente):
+def insert_spent(option):
     valor_saida = float(input('Valor da saida: '))
     data_saida = str(input('Data da saida: '))
-    data_format1 = datetime.strptime(data_saida, '%d-%m-%Y')
     nome_saida = str(input('Descrição da saida: '))
-    recorrente_saida = str(input('recorrente: ')).strip().upper()
+    recorrente_saida = str(input('recorrente [S/N]: ')).strip().upper()
     if recorrente_saida in 'sS':
         recorrente_saida = 'sim'
     else:
         recorrente_saida = 'nao'
-    cursor = con.cursor()
-    saida_sql = 'INSERT INTO SAIDAS (valor, vencimento, nome, recorrente) values (%s, %s, %s, %s)'
-    input_saida = (valor_saida, data_format1, nome_saida, recorrente_saida)
-    cursor.execute(saida_sql, input_saida)
-    con.commit()
-    print('valores de saídas inseridos com sucesso!')
-    cursor.close()
+    tabela = ContentTable()
+    tabela.inputData(valor_saida, nome_saida, data_saida, recorrente_saida)
+    diretorio = ('spent.csv')
+    if os.path.isfile(diretorio) == False:
+        with open('spent.csv', 'w') as f:
+            f.write(','.join(coloumns_names))
+            f.write('\n')
+            f.close()
+    csvFormat = pd.read_csv('spent.csv', sep=',')
+    csvFormat = pd.concat([csvFormat, pd.DataFrame(
+        [tabela.getListFormat()], columns=coloumns_names)])
+    csvFormat.to_csv('spent.csv', index=False)
+    print('valores de saídas inseridos com sucesso!'
+          )
 
 
-def insere_pagamento(con, valor, data, descricao, id_saidas, id_entradas, pago):
+def insert_payment(option):
     valor_pagamento = float(input('Valor do pagamento: '))
     data_pagamento = str(input('Data do pagamento: '))
-    data_format_pagamento = datetime.strptime(data_pagamento, '%d-%m-%Y')
     descricao_pagamento = str(input('Descrição do pagamento: '))
-    input_id_saidas = int(input('ID da saida: '))
-    input_id_entradas = int(input('ID da entrada:'))
-    input_pago = str(input('Pago [sim/nao]: '))
-    cursor = con.cursor()
-    pagamento_sql = 'INSERT INTO pagamento (valor, data, descricao, id_saidas, id_entradas, pago) values (%s, %s, %s, %s, %s, %s)'
-    input_pagamento = (valor_pagamento, data_format_pagamento,descricao_pagamento, input_id_saidas, input_id_entradas, input_pago)
-    cursor.execute(pagamento_sql, input_pagamento)
-    con.commit()
-    print('valores de saídas inseridos com sucesso!')
-    cursor.close()
-
-def consulta_entradas(con, valor, data, nome, recorrente, id_entradas):
-    cursor = con.cursor()
-    cursor.execute("SELECT * FROM entradas")
-    colunas = cursor.fetchall()
-    for coluna in colunas:
-        print(valor, 'valor R$:', coluna[0], end='')
-        print(data, '|', 'data:', coluna[1], end='')
-        print(nome, '|', 'nome:', coluna[2], end='')
-        print(recorrente, '|', 'recorrente', coluna[3], end='')
-        print(id_entradas, '|', 'ID entradas:', coluna[4], '\n')
-        print('▀' * 95)
+    input_pago = str(input('Pago [S/N]: ')).strip().upper()
+    if input_pago in 'Ss':
+        input_pago = 'sim'
+    else:
+        input_pago = 'nao'
+    tabela = ContentTable()
+    tabela.inputData(valor_pagamento, descricao_pagamento,
+                     data_pagamento, input_pago)
+    diretorio = ('payment.csv')
+    if os.path.isfile(diretorio) == False:
+        with open('payment.csv', 'w') as f:
+            f.write(','.join(coloumns_names2))
+            f.write('\n')
+            f.close()
+    csvFormat = pd.read_csv('payment.csv', sep=',')
+    csvFormat = pd.concat([csvFormat, pd.DataFrame(
+        [tabela.getListFormat()], columns=coloumns_names2)])
+    csvFormat.to_csv('payment.csv', index=False)
+    print('valores de pagamento inseridos com sucesso!')
 
 
-def consulta_saidas(con, valor, vencimento, nome, recorrente):
-    input_filtro_saidas = str(input('Nome da conta: '))
-    consulta_saidas1 = ("SELECT * FROM saidas WHERE nome='"+input_filtro_saidas+"'")
-    cursor = con.cursor()
-    cursor.execute(consulta_saidas1)
-    colunas = cursor.fetchall()
-    for coluna in colunas:
-        print(valor, 'valor:', coluna[0], 'R$')
-        print(vencimento, coluna[1])
-        print(nome, 'nome:', coluna[2])
-        print(recorrente, coluna[3])
+def query_compensation(option):
+    consulta_entradas1 = str(input('Entrada desejada: '))
+    with open('compensation.csv', 'r') as f:
+        exibir = f.readlines()
+    print('Valor | Nome | Data | Recorrente')
+    for linha in exibir:
+        if consulta_entradas1 in linha:
+            print(linha)
 
 
-def consulta_todas_saidas(con, valor, vencimento, nome, recorrente, id_saidas):
-    consulta_saidas1 = 'SELECT * FROM saidas'
-    cursor = con.cursor()
-    cursor.execute(consulta_saidas1)
-    colunas = cursor.fetchall()
-    for coluna in colunas:
-        print(valor, 'valor R$:', coluna[0], end='')
-        print(vencimento, '|', 'vencimento:', coluna[1], end='')
-        print(nome, '|', 'nome:', coluna[2], end='')
-        print(recorrente, '|', 'recorrente:', coluna[3], end='')
-        print(id_saidas, '|', 'ID saidas:', coluna[4], '\n')
-        print('▀' * 95)
+def query_spent(option):
+    consulta_saidas1 = str(input('Saída desejada: '))
+    with open('spent.csv', 'r') as f:
+        exibir = f.readlines()
+    print('Valor | Nome | Data | Recorrente')
+    for linha in exibir:
+        if consulta_saidas1 in linha:
+            print(linha)
 
 
-def consulta_pagamentos(con, valor, data, descricao, id_saidas, id_entradas, pago):
-    id_entrada_pagamento = str(input('Digite o ID da entrada: '))
-    pagamento_sql1 = "SELECT * FROM pagamento WHERE id_entradas=" + id_entrada_pagamento
-    cursor = con.cursor()
-    cursor.execute(pagamento_sql1)
-    colunas = cursor.fetchall()
-    for coluna in colunas:
-        print(valor, 'valor R$:', coluna[0], end='')
-        print(data, '|', 'data:', coluna[1], end='')
-        print(descricao, '|', 'descrição:', coluna[2], end='')
-        print(id_saidas, '|', 'ID saidas', coluna[3], end='')
-        print(id_entradas, '|', 'ID entradas:', coluna[4], end='')
-        print(pago, '|', 'pago:', coluna[5], '\n')
-        print('▀' * 95)
+def query_payment (option):
+    consulta_payment = str(input('Pagamento desejado: '))
+    with open('payment.csv', 'r') as f:
+        exibir = f.readlines()
+    print('Valor | Nome | Data | Pago')
+    for linha in exibir:
+        if consulta_payment in linha:
+            print(linha)
 
-
-def calculo(con, valor):
-    calculo_input = str(input('ID de entrada para a soma: '))
-    calculo_sql = 'SELECT SUM(valor) FROM pagamento WHERE id_entradas=' + calculo_input 
-    cursor = con.cursor()
-    cursor.execute(calculo_sql)
-    colunas = cursor.fetchall()
-    for coluna in colunas:
-        print(valor,coluna[0])
 
 
 def main():
-
+    # TODO verify if the file data and spent files already exists
+    # if not create both with columns names
+    # input (user_input, self.user_name, self.user_date, self.user_continuous)
+    # output (self.user_output, self.user_name, self.user_date, self.user_continuous)
     while True:
-        con = criar_conexao("localhost", "root", "", "calcula_python")
+        
         print('''Digite a opção desejada:
         [1] inserir entradas
         [2] inserir saidas
         [3] inserir pagamento
         [4] consultar entradas
-        [5] consulta todas as saidas
-        [6] consultar saidas pelo nome
-        [7] consultar pagamentos por ID de entrada
-        [8] calculo total por ID de entrada
-        [9] sair do programa
+        [5] consultar saidas
+        [6] consultar pagamentos
+        
         ''')
+        options = {
+            1: insert_compensation,
+            2: insert_spent,
+            3: insert_payment,
+            4: query_compensation,
+            5: query_spent,
+            6: query_payment
 
-        opcao = int(input('Qual opção você deseja? '))
-        if opcao == 1:
-            insere_entradas(con, "", "", "", "")
-        elif opcao == 2:
-            insere_saidas(con, '', '', '', '')
-        elif opcao == 3:
-            insere_pagamento(con, '', '', '', '', '','')
-        elif opcao == 4:
-            consulta_entradas(con, '', '', '', '','')
-        elif opcao == 5:
-            consulta_todas_saidas(con, '', '', '', '', '')
-        elif opcao == 6:
-            consulta_saidas(con, '', '', '', '')
-        elif opcao == 7:
-            consulta_pagamentos(con, '', '', '', '', '', '')
-        elif opcao == 8:
-           calculo(con,'')
-        elif opcao == 9:
-            print('Obrigado, Volte sempre!')
-            break
+        }
 
-        fechar_conexao(con)
-
-
-
-
+        option = int(input('Which option would you like to perform? '))
+        options.get(option)(option)
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
